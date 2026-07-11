ea26703abb0a6e3b51ad8220481c04ff7e5ff3de
HanZhengping97 <dudu19970207@outlook.com>
2026-07-12 00:38:50 +0200

    Export physical permeance API

--- src/mrn/__init__.py
+++ src/mrn/__init__.py
@@ -1,5 +1,10 @@
 """Magnetic reluctance network matrix assembly helpers."""
 
+from .geometry_permeance import (
+    calculate_branch_permeance,
+    calculate_mesh_branch_permeances,
+    find_adjacent_cells,
+)
 from .nodal_matrix import apply_reference_node, build_nodal_permeance_matrix, remove_reference_entry
 from .permeance import BranchPermeance, build_branch_permeance_matrix
 
@@ -8,5 +13,8 @@ __all__ = [
     "apply_reference_node",
     "build_branch_permeance_matrix",
     "build_nodal_permeance_matrix",
+    "calculate_branch_permeance",
+    "calculate_mesh_branch_permeances",
+    "find_adjacent_cells",
     "remove_reference_entry",
 ]
